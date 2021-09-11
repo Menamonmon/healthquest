a3c8d4f6ab2720f5ed51149cee5ac802a53fb099
menamonmon <monyasser111@gmail.com>
2021-09-11 15:17:17 -0400

    created reminders page

--- src/pages/RemindersPage.tsx
+++ src/pages/RemindersPage.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { IconButton } from "@chakra-ui/button";
+import { Box, List } from "@chakra-ui/layout";
+import React, { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import ReminderItem from "../components/ReminderItem";
+import { getMyReminders } from "../services/firebase";
+import { Reminder } from "../types";
+import { Link as RouterLink } from "react-router-dom";
 
 const RemindersPage = () => {
-  return <div>REMINDERS PAGE</div>;
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  const fetchReminders = async () => {
+    const myReminders = await getMyReminders();
+    setReminders(myReminders);
+  };
+
+  useEffect(() => {
+    fetchReminders();
+  }, []);
+
+  return (
+    <Box>
+      <List>
+        {reminders.map((reminder, idx) => (
+          <ReminderItem reminder={reminder} key={idx} />
+        ))}
+      </List>
+      <IconButton
+        size="lg"
+        colorScheme="green"
+        rounded="full"
+        position="absolute"
+        bottom="5%"
+        right="5%"
+        aria-label="add-reminder"
+        as={RouterLink}
+        to="/add-reminder"
+      >
+        <FaPlus></FaPlus>
+      </IconButton>
+    </Box>
+  );
 };
 
 export default RemindersPage;
