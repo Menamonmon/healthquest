bdc1114353e95363a62ac265cfc26205dc49d1f2
menamonmon <monyasser111@gmail.com>
2021-10-23 10:25:46 -0400

    a page for a specific conversation

--- src/pages/ChatConversationPage.tsx
+++ src/pages/ChatConversationPage.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Box } from "@chakra-ui/layout";
+import React, { useCallback, useEffect, useState } from "react";
+import { getChatConversationInfo } from "../services/firebase";
+import { ChatConversationInfo } from "../types";
+import { getParamFromUrl } from "../utils";
 
-const ChatConversationPage = () => {
-  return <div></div>;
+const ChatConversationPage: React.FC = () => {
+  const [convId, setConvId] = useState("");
+  const [convInfo, setConvInfo] = useState<ChatConversationInfo | null>(null);
+
+  const fetchConvInfo = useCallback(async () => {
+    if (convId.trim() !== "") {
+      const info = await getChatConversationInfo(convId);
+      setConvInfo(info);
+    }
+  }, [convId]);
+
+  useEffect(() => {
+    setConvId(getParamFromUrl("conversation_id"));
+  }, []);
+
+  useEffect(() => {
+    fetchConvInfo();
+  }, [fetchConvInfo]);
+
+  return <Box>{JSON.stringify(convInfo)}</Box>;
 };
 
 export default ChatConversationPage;
