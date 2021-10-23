9e15d44017e8e6c30949c2155b7c6f5b1cfce1f0
menamonmon <monyasser111@gmail.com>
2021-10-23 14:33:47 -0400

    created chat conv page

--- src/pages/ChatConversationPage.tsx
+++ src/pages/ChatConversationPage.tsx
@@ -1,12 +1,21 @@
-import { Box } from "@chakra-ui/layout";
-import React, { useCallback, useEffect, useState } from "react";
-import { getChatConversationInfo } from "../services/firebase";
+import { Box, Flex, List, Heading } from "@chakra-ui/layout";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { firestore, getChatConversationInfo } from "../services/firebase";
 import { ChatConversationInfo } from "../types";
 import { getParamFromUrl } from "../utils";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import ChatMessage from "../components/ChatMessage";
+import ChatInputBox from "../components/ChatInputBox";
+import { BackFooter } from "../components/BackFooter";
 
 const ChatConversationPage: React.FC = () => {
-  const [convId, setConvId] = useState("");
+  const convId = getParamFromUrl("conversation_id");
   const [convInfo, setConvInfo] = useState<ChatConversationInfo | null>(null);
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef
+    .where("conv_id", "==", convId)
+    .orderBy("created_at");
+  const [messages] = useCollectionData(query, { idField: "id" });
 
   const fetchConvInfo = useCallback(async () => {
     if (convId.trim() !== "") {
@@ -14,16 +23,43 @@ const ChatConversationPage: React.FC = () => {
       setConvInfo(info);
     }
   }, [convId]);
-
-  useEffect(() => {
-    setConvId(getParamFromUrl("conversation_id"));
-  }, []);
+  const feedRef: any = useRef<HTMLUListElement>();
+  const scrollToBottom = useCallback(() => {
+    if (feedRef.current) {
+      feedRef.current.scrollTop = feedRef.current.scrollHeight;
+    }
+  }, [feedRef]);
 
   useEffect(() => {
     fetchConvInfo();
   }, [fetchConvInfo]);
 
-  return <Box>{JSON.stringify(convInfo)}</Box>;
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <Flex flexDir="column" h="97%">
+      <List
+        flexGrow={1}
+        ref={feedRef}
+        overflowY="auto"
+        maxHeight="calc(100vh - 100px)"
+      >
+        {!messages || messages.length === 0 ? (
+          <Heading>
+            No messages to display. Type your first message below!
+          </Heading>
+        ) : (
+          messages.map((msg, idx) => <ChatMessage key={idx} msg={msg} />)
+        )}
+      </List>
+      <Box>
+        <ChatInputBox convId={convId} />
+        <BackFooter />
+      </Box>
+    </Flex>
+  );
 };
 
 export default ChatConversationPage;
