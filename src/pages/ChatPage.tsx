f64c2a341124af6d7f0fc78d9609efe6ef7666e8
menamonmon <monyasser111@gmail.com>
2021-10-23 10:25:33 -0400

    a chat page that has the search bar and lists all of the conversations

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Box } from "@chakra-ui/layout";
+import { BackFooter } from "../components/BackFooter";
+import React, { useState } from "react";
+import ChatSearchBox from "../components/ChatSearchBox";
+import ChatConversationsList from "../components/ChatConversationsList";
+import { ChatConversation } from "../types";
 
 const ChatPage = () => {
-  return <div>CHAT PAGE</div>;
+  const [conversations, setConversations] = useState<ChatConversation[]>([]);
+
+  return (
+    <Box display="flex" justifyContent="space-between" flexDir="column">
+      <Box flexGrow={1}>
+        <ChatSearchBox setConversations={setConversations} />
+        <ChatConversationsList
+          conversations={conversations}
+          setConversations={setConversations}
+        />
+      </Box>
+      <BackFooter />
+    </Box>
+  );
 };
 
 export default ChatPage;
